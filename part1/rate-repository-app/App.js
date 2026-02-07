62a2545908433c387606f6948f30482ef87791fe
Pedro Araújo <pedroprog@gmail.com>
2026-02-07 17:52:06 +0000

    Solution to Exercise 10.3: the reviewed repositories list

--- part1/rate-repository-app/App.js
+++ part1/rate-repository-app/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RepositoryList from './components/RepositoryList'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+   /*  <View style={styles.container}>
       <Text>Open up App.js to start working on your app! Test</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> */
+    <RepositoryList/>
   );
 }
 
---
+++ part1/rate-repository-app/components/RepositoryItem.jsx
@@ -0,0 +1,24 @@
+import {View, StyleSheet, Text} from 'react-native';
+
+const styles = StyleSheet.create({
+   content: {
+    padding: 16,
+    gap: 4,
+  },
+  });
+
+const RepositoryItem = ({ item }) => {
+  return (
+    <View style={styles.content}>
+      <Text>Full name: {item.fullName}</Text>
+      <Text>Description: {item.description}</Text>
+      <Text>Language: {item.language}</Text>
+      <Text>Stars: {item.stargazersCount}</Text>
+      <Text>Forks: {item.forksCount}</Text>
+      <Text>Reviews: {item.reviewCount}</Text>
+      <Text>Rating: {item.ratingAverage}</Text>
+    </View>
+  )
+}
+
+export default RepositoryItem
